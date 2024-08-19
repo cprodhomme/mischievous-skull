d56a9dead093700006cf6e9efa034c81794e0af5
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-08-19 13:46:39 +0200

    configure skull

--- src/main/java/com/cprodhomme/mischievousskull/block/ModBlocks.java
+++ src/main/java/com/cprodhomme/mischievousskull/block/ModBlocks.java
@@ -1,10 +1,8 @@
 package com.cprodhomme.mischievousskull.block;
 
 import com.cprodhomme.mischievousskull.Mischievousskull;
-import com.cprodhomme.mischievousskull.block.entity.MischievousSkullBlockEntity;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -12,39 +10,26 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-  public static final Block MISCHIEVOUS_SKULL = registerBlock(
+  public static final Block MISCHIEVOUS_SKULL = register(
+    MischievousSkullBlock.MISCHIEVOUS_SKULL_BLOCK,
     MischievousSkullBlock.IDENTIFIER,
-    MischievousSkullBlock.MISCHIEVOUS_SKULL_BLOCK
-  );
-  public static final Block MISCHIEVOUS_WALL_SKULL = registerBlock(
-    MischievousWallSkullBlock.IDENTIFIER,
-    MischievousWallSkullBlock.MISCHIEVOUS_SKULL_BLOCK
+    true
   );
 
-  public static final BlockEntityType<MischievousSkullBlockEntity> MISCHIEVOUS_SKULL_BLOCK_ENTITY = Registry.register(
-    Registries.BLOCK_ENTITY_TYPE,
-    new Identifier(Mischievousskull.MOD_ID, "mischievous_skull_block_entity"),
-    BlockEntityType.Builder.create(MischievousSkullBlockEntity::new, MischievousSkullBlock.MISCHIEVOUS_SKULL_BLOCK).build()
-  );
+  private static Block register(Block block, String name, boolean shouldRegisterItem) {
+		// Register the block and its item.
+		Identifier id = Identifier.of(Mischievousskull.MOD_ID, name);
 
-  private static Block registerBlock(String name, Block block) {
-    return Registry.register(
-      Registries.BLOCK,
-      new Identifier(Mischievousskull.MOD_ID, name),
-      block
-    );
-  }
+		// Sometimes, you may not want to register an item for the block.
+		// Eg: if it's a technical block like `minecraft:air` or `minecraft:end_gateway`
+		if (shouldRegisterItem) {
+			BlockItem blockItem = new BlockItem(block, new Item.Settings());
+			Registry.register(Registries.ITEM, id, blockItem);
+		}
 
-  @SuppressWarnings("unused")
-  private static Item registerBlockItem(String name, Block block) {
-    return Registry.register(
-      Registries.ITEM,
-      new Identifier(Mischievousskull.MOD_ID, name),
-      new BlockItem(block, new Item.Settings())
-    );
-  }
+		return Registry.register(Registries.BLOCK, id, block);
+	}
 
   public static void initialize() {
-    Mischievousskull.LOGGER.debug("Registrering ModBlocks for " + Mischievousskull.MOD_ID);
   }
 }
