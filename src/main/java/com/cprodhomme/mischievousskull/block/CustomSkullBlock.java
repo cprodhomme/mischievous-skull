d9e4893ea80e2ce2553686c51aacc1bd7d9c7ba8
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-08-20 20:58:26 +0200

    ✨ Skull can be placed in 16 directions

--- src/main/java/com/cprodhomme/mischievousskull/block/CustomSkullBlock.java
+++ src/main/java/com/cprodhomme/mischievousskull/block/CustomSkullBlock.java
@@ -3,28 +3,28 @@ package com.cprodhomme.mischievousskull.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.RotationPropertyHelper;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 
 public class CustomSkullBlock extends Block {
-  public static final DirectionProperty FACING = DirectionProperty.of("facing", Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST);
+  public static final int MAX_ROTATION_INDEX = RotationPropertyHelper.getMax();
+	private static final int MAX_ROTATIONS = MAX_ROTATION_INDEX + 1;
+	public static final IntProperty ROTATION = Properties.ROTATION;
 
   private static final VoxelShape SHAPE = VoxelShapes.cuboid(0.25, 0.0, 0.25, 0.75, 0.5, 0.75);
 
   public CustomSkullBlock(Settings settings) {
     super(settings);
-    this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+    this.setDefaultState(this.getDefaultState().with(ROTATION, Integer.valueOf(0)));
   }
 
   @Override
@@ -33,32 +33,28 @@ public class CustomSkullBlock extends Block {
   }
 
   @Override
-  protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-    builder.add(FACING);
-  }
-  
-  @Override
-  public BlockState getPlacementState(ItemPlacementContext ctx) {
-    Direction playerFacing = ctx.getHorizontalPlayerFacing().getOpposite();
-    return this.getDefaultState().with(FACING, playerFacing);
-  }
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return super.getPlacementState(ctx).with(ROTATION, Integer.valueOf(RotationPropertyHelper.fromYaw(ctx.getPlayerYaw())));
+	}
+
+	@Override
+	protected BlockState rotate(BlockState state, BlockRotation rotation) {
+		return state.with(ROTATION, Integer.valueOf(rotation.rotate((Integer)state.get(ROTATION), MAX_ROTATIONS)));
+	}
+
+	@Override
+	protected BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.with(ROTATION, Integer.valueOf(mirror.mirror((Integer)state.get(ROTATION), MAX_ROTATIONS)));
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		super.appendProperties(builder);
+		builder.add(ROTATION);
+	}
 
   @Override
   protected VoxelShape getCullingShape(BlockState state, BlockView world, BlockPos pos) {
     return VoxelShapes.empty();
-  }
-
-  protected BlockState rotate(BlockState state, BlockRotation rotation) {
-    return (BlockState)state.with(FACING, rotation.rotate((Direction)state.get(FACING)));
-  }
-
-  protected BlockState mirror(BlockState state, BlockMirror mirror) {
-    return state.rotate(mirror.getRotation((Direction)state.get(FACING)));
-  }
-
-  @Override
-  public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-    Direction facing = placer.getHorizontalFacing().rotateYClockwise();  // rotation à 45 degrés pour simuler les diagonales
-    world.setBlockState(pos, state.with(FACING, facing), 2);
   }
 }
